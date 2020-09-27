97d3440228cc16e01566814f626af8351d2534ef
Philip Yap <philipyap@Philips-MacBook.local>
2020-09-26 17:55:12 -0700

    [home.html]add form

--- main_app/views.py
+++ main_app/views.py
@@ -1,16 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Category, Product
 
 # Create your views here.
 
-##### PRODUCTS #####
-def product_list(request):
-  categories = Category.objects.all()
-  products = Product.objects.filter(name)
-  return render(request, 'home.html', {'categories': categories, 'products': products})
-
-##### DEFAULTS #####
+def home(request):
+  return render(request, 'home.html')
 
 def about(request):
  return render(request, 'about.html')
