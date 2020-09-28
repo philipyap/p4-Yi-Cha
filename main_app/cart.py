b5b45697c0e5149059c03a7871990d84bfc0c002
Irene <airine@Irenes-MacBook-Air.local>
2020-09-28 11:53:48 -0700

    [views, cart, urls] Add order_create request in views.py. Add OrderCreateForm in cart.py as a checkout form used. Add path order_create

--- main_app/cart.py
+++ main_app/cart.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from decimal import Decimal
-from .models import Product
+from .models import Product, Order
 from django import forms
 
 class Cart(object):
@@ -61,4 +61,9 @@ PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 class CartAddProductForm(forms.Form):
     quantity = forms.TypedChoiceField(choices=PRODUCT_QUANTITY_CHOICES,coerce=int)
-    update = forms.BooleanField(required=False,initial=False,widget=forms.HiddenInput)  
+    update = forms.BooleanField(required=False,initial=False,widget=forms.HiddenInput)
+
+ class OrderCreateForm(forms.ModelForm):
+     class Meta:
+     model = Order
+     fields = ['first_name', 'last_name', 'shipping_address','email']
