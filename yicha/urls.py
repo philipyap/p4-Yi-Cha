94c2e56dd99e7e88b62e7e734f1a744e18285a5a
Philip Yap <philipyap@Philips-MacBook.local>
2020-09-28 12:19:45 -0700

    [image] added image.url in settings.py and image in product

--- yicha/urls.py
+++ yicha/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.urls import path, include
 from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Add the line below...
     path('', include('main_app.urls')),
 ]
+
+# set the path to media url
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
