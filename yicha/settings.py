0531a1497ed7d185b6a7035d3c0873eb3a6d0b9e
Philip Yap <philipyap@Philips-MacBook.local>
2020-10-01 12:29:36 -0700

    [settings]add config and set DATABASE_URL

--- yicha/settings.py
+++ yicha/settings.py
@@ -9,8 +9,34 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import os
 from pathlib import Path
+from decouple import config
+
+import os
+import socket
+import psycopg2
+import dj_database_url
+
+
+DATABASE_URL =  os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
+# If the host name starts with 'live', DJANGO_HOST = "production"
+if socket.gethostname().startswith('live'):
+    DJANGO_HOST = "production"
+# Else if host name starts with 'test', set DJANGO_HOST = "test"
+elif socket.gethostname().startswith('test'): 
+    DJANGO_HOST = "testing"
+else:
+# If host doesn't match, assume it's a development server, set DJANGO_HOST = "development"
+    DJANGO_HOST = "development"
+# Define general behavior variables for DJANGO_HOST and all others
+if DJANGO_HOST == "production":
+    DEBUG = False
+    STATIC_URL = 'https://yourappname.herokuapp.com'
+else:
+    DEBUG = True
+    STATIC_URL = '/static/'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +46,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm+w7c8_fnnk!=_y50j+6=3lq88%eaq^yk^ur$#on0#3m22qqr6'
+SECRET_KEY = os.environ['SECRET_KEY'] or config('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -44,6 +68,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -57,7 +82,9 @@ ROOT_URLCONF = 'yicha.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            '/www/STORE/main_app/templates/',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,13 +103,15 @@ WSGI_APPLICATION = 'yicha.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# development
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'yicha',
     }
 }
-
+# production
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -120,7 +149,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
 
 CART_SESSION_ID = 'cart'
 
@@ -129,3 +157,28 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 MEDIA_URL ='/images/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+# Define EMAIL_BACKEND variable for DJANGO_HOST
+if DJANGO_HOST == "production":
+    # Output to SMTP server on DJANGO_HOST production
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+elif DJANGO_HOST == "testing":
+    # Nullify output on DJANGO_HOST test
+    EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+else: 
+    # Output to console for all others
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# Define CACHES variable for DJANGO_HOST production and all other hosts 
+if DJANGO_HOST == "production":
+   # Set cache
+   CACHES = {
+        'default': {
+            'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+            'LOCATION': '127.0.0.1:11211',
+            'TIMEOUT':'1800',
+            }
+        }
+   CACHE_MIDDLEWARE_SECONDS = 1800
+else: 
+   # No cache for all other hosts
+   pass
